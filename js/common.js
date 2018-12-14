bfdb1672fe587529d579e7b969dc41b32368b8a8
Andriy <andriydzikh@gmail.com>
2018-12-14 18:08:43 -0500

    Rewrote common.js to use better Promise practices. Also using Fetch API instead of XmlHttpRequests

--- js/common.js
+++ js/common.js
@@ -56,148 +56,130 @@ function clearStoredDeviceName()
 
 function updateDeviceList()
 {
-    return new Promise(function(resolve, reject){
-        GetFileList().then(function(items){
-            var dev = new Array();
-            items.forEach(function(elem){
-                dev.push({name: elem.title, id: elem.id});
-            });
-            resolve(dev);
-        }, function(err){
-            $('#deviceList').text("<span style='color: red'>Failed to update the device list.</span>");
-            reject(err);
+    return GetFileList().then(function(items){
+        const dev = new Array();
+        items.forEach(function(elem){
+            dev.push({name: elem.title, id: elem.id});
         });
+        return Promise.resolve(dev);
+    }, function(err){
+        $('#deviceList').text("<span style='color: red'>Failed to update the device list.</span>");
+        return Promise.reject(err);
     });
 }
 
 // This assumes that access token is valid.
 function CreateFileOnDrive(filename, fileData, mimetype)
 {
-    return new Promise(function(resolve, reject){
-        var ajax = new XMLHttpRequest();
-        ajax.onreadystatechange = function() {
-            if(this.readyState == 4){
-                if(this.status == 200)
-                {
-                    resolve(JSON.parse(this.responseText));
-                }
-                else
-                {
-                    reject(Error(JSON.stringify({status: this.status, response: this.responseText})));
-                }
-            }
-        };
+    let uploadBody = "";
+    uploadBody += "--UploadBoundary\n";
+    uploadBody += "Content-type: application/json; charset=UTF-8\n\n";
+    uploadBody += JSON.stringify({name: filename, parents: ['appDataFolder']});
+    uploadBody += "\n--UploadBoundary\n";
+    uploadBody += "Content-Type: " + mimetype + "\n\n";
+    uploadBody += fileData;
+    uploadBody += "\n--UploadBoundary--";
 
-        var uploadBody = "";
-        uploadBody += "--UploadBoundary\n";
-        uploadBody += "Content-type: application/json; charset=UTF-8\n\n";
-        uploadBody += JSON.stringify({name: filename, parents: ['appDataFolder']});
-        uploadBody += "\n--UploadBoundary\n";
-        uploadBody += "Content-Type: " + mimetype + "\n\n";
-        uploadBody += fileData;
-        uploadBody += "\n--UploadBoundary--";
-
-        ajax.open("POST",
-            "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart", true);
-        ajax.setRequestHeader('Content-Type', 'multipart/related; boundary=UploadBoundary');
-        ajax.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-        ajax.send(uploadBody);
+    let status;
+    return fetch("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart", {
+        method: "POST",
+        body: uploadBody,
+        headers: {
+            "Content-Type": "multipart/related; boundary=UploadBoundary",
+            'Authorization': 'Bearer ' + accessToken
+        }
+    }).then(response => {
+        status = response.status;
+        return response.json();
+    }).then(json => {
+        if(status == 200) {
+            return Promise.resolve(json);
+        } else {
+            return Promise.reject({status, response: json});
+        }
     });
 }
 
 function UpdateFileOnDrive(fileId, data, mimeType)
 {
-    return new Promise(function(resolve, reject){
-        var ajax = new XMLHttpRequest();
-        ajax.onreadystatechange = function() {
-            if(this.readyState == 4){
-                if(this.status == 200)
-                {
-                    resolve(JSON.parse(this.responseText));
-                }
-                else
-                {
-                    reject(Error(JSON.stringify({status: this.status, response: this.responseText})));
-                }
-            }
-        };
-
-        ajax.open("PUT",
-            "https://www.googleapis.com/upload/drive/v2/files/" + fileId + "?uploadType=media", true);
-        ajax.setRequestHeader('Content-Type', mimeType);
-        ajax.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-        ajax.send(data);
+    let status;
+    return fetch("https://www.googleapis.com/upload/drive/v2/files/" + fileId + "?uploadType=media", {
+        method: "PUT",
+        body: data,
+        headers: {
+            "Content-Type": mimeType,
+            'Authorization': 'Bearer ' + accessToken
+        }
+    }).then(response => {
+        status = response.status;
+        return response.json();
+    }).then(json => {
+        if(status == 200) {
+            return Promise.resolve(json);
+        } else {
+            return Promise.reject({status, response: json});
+        }
     });
 }
 
 function DeleteFileOnDrive(fileId)
 {
-    return new Promise(function(resolve, reject){
-        var ajax = new XMLHttpRequest();
-        ajax.onreadystatechange = function() {
-            if(this.readyState == 4){
-                if(this.status == 200)
-                {
-                    resolve(JSON.parse(this.responseText));
-                }
-                else
-                {
-                    reject(Error(JSON.stringify({status: this.status, response: this.responseText})));
-                }
-            }
-        };
-
-        ajax.open("DELETE",
-            "https://www.googleapis.com/drive/v2/files/" + fileId, true);
-        ajax.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-        ajax.send();
+    let status;
+    return fetch("https://www.googleapis.com/drive/v2/files/" + fileId, {
+        method: "DELETE",
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        }
+    }).then(response => {
+        status = response.status;
+        return response.json();
+    }).then(json => {
+        if(status == 200) {
+            return Promise.resolve(json);
+        } else {
+            return Promise.reject({status, response: json});
+        }
     });
 }
 
 // This assumes that access token is valid.
 function GetFileList(onsuccess, onerror)
 {
-    return new Promise(function(resolve, reject){
-        var ajax = new XMLHttpRequest();
-        ajax.onreadystatechange = function() {
-            if(this.readyState == 4){
-                if(this.status == 200)
-                {
-                    resolve(JSON.parse(this.responseText).items);
-                }
-                else
-                {
-                    reject(Error(JSON.stringify({status: this.status, response: this.responseText})));
-                }
-            }
-        };
-        
-        ajax.open("GET", "https://www.googleapis.com/drive/v2/files", true);
-        ajax.setRequestHeader('spaces', 'appDataFolder');
-        ajax.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-        ajax.send();
+    let status;
+    return fetch("https://www.googleapis.com/drive/v2/files" + fileId, {
+        method: "GET",
+        headers: {
+            'spaces': 'appDataFolder',
+            'Authorization': 'Bearer ' + accessToken
+        }
+    }).then(response => {
+        status = response.status;
+        return response.json();
+    }).then(json => {
+        if(status == 200) {
+            return Promise.resolve(json.items);
+        } else {
+            return Promise.reject({status, response: json});
+        }
     });
 }
 
 function GetFileData(fileId)
 {
-    return new Promise(function(resolve, reject){
-        var ajax = new XMLHttpRequest();
-        ajax.onreadystatechange = function() {
-            if(this.readyState == 4){
-                if(this.status == 200)
-                {
-                    resolve(this.responseText);
-                }
-                else
-                {
-                    reject(Error(JSON.stringify({status: this.status, response: this.responseText})));
-                }
-            }
-        };
-        
-        ajax.open("GET", "https://www.googleapis.com/drive/v2/files/" + fileId + "?alt=media", true);
-        ajax.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-        ajax.send();
+    let status;
+    return fetch("https://www.googleapis.com/drive/v2/files/" + fileId + "?alt=media", {
+        method: "GET",
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        }
+    }).then(response => {
+        status = response.status;
+        return response.text();
+    }).then(text => {
+        if(status == 200) {
+            return Promise.resolve(text);
+        } else {
+            return Promise.reject({status, response: text});
+        }
     });
 }
